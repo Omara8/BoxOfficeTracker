7dee07419c18e3cb3e400b0f2420ab8a3e0d635d
Mahmoud.Omara <Mahmoud.Omara@itworx.com>
2021-09-15 18:32:35 +0200

    added the support for the bottom nav

--- app/src/main/java/com/planatech/bo/tracker/MainActivity.kt
+++ app/src/main/java/com/planatech/bo/tracker/MainActivity.kt
@@ -2,12 +2,26 @@ package com.planatech.bo.tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var bottomNavigation: BottomNavigationView? = null
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        navController = Navigation.findNavController(this, R.id.fragment_container)
+        navController?.let {
+            bottomNavigation?.setupWithNavController(it)
+        }
     }
 }
